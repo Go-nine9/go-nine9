203a68c0fed7ea15eab8fd6f7b6c8edf8678a41a
Marie <mje.espinosa@gmail.com>
2024-02-07 18:54:25 +0100

    fix routing

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,69 +1,51 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
-import { useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Routes, Route, BrowserRouter, useNavigate } from 'react-router-dom';
 import { AuthContext, AuthProvider } from './AuthContext/AuthContext';
 import Login from './pages/login/Login';
-import { Routes, Route, BrowserRouter, useNavigate } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Register from './pages/register/Register';
 import Dashboard from './pages/Manager/Dashboard';
 import CreateSalons from './pages/Manager/CreateSalons';
 
-function Layout() {
-  const { isAuthenticated, isManager, logout} = useContext(AuthContext);
-  const navigate = useNavigate();
-  console.log(isManager)
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      navigate('/login');
-    }
-  }, [isAuthenticated, navigate]);
-
-  if (!isAuthenticated) {
-    return null;
-  }
-
-  return (
-    <div>
-      <section>
-      <header>
-      <h1> Planity </h1>
-        <button onClick={logout}> Se déconnecter </button>
-      </header>
-        <Routes>
-          <Route path="/" element={isManager ? <Staff />: <Home />} />
-        </Routes>
-      </section>
-    </div>
-  );
-}
-
-function Staff(){
-
-  return(
-    <Routes>
-    <Route path="/admin" element={Dashboard} />
-          <Route path="/admin/create" element={CreateSalons} />
-          
-    </Routes>
-  )
-
-}
-
 function App() {
+  const { isAuthenticated, isManager } = useContext(AuthContext);
+
+  const ManagerRoutes = (
+    <Routes>
+        <Route path="/admin/*" element={<StaffRoutes />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+
+  const publicRoutes = (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/*" element={<Login />} />
+    </Routes>
+  );
+
+  const Layout = () => {
+    return (
+      <div>
+        <BrowserRouter>
+          <AuthProvider>
+            {isManager ? ManagerRoutes : isAuthenticated ? publicRoutes : publicRoutes}
+          </AuthProvider>
+        </BrowserRouter>
+      </div>
+    );
+  };
+
+  return <Layout />;
+}
+
+function StaffRoutes() {
   return (
-    <div>
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/*" element={<Layout />} />
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </div>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/create" element={<CreateSalons />} />
+    </Routes>
   );
 }
 
